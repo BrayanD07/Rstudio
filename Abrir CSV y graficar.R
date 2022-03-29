ef43e0c971f02b8ad270a5f51e096b00215c35ec
BrayanD07 <brayaneve@hotmail.com>
2022-03-29 09:51:10 -0600

    update

--- Abrir CSV y graficar.R
+++ Abrir CSV y graficar.R
@@ -6,5 +6,10 @@ colnames(datosEntr)=c("fecha","abrio","maximo","minimo","cerra","adjcerra","volu
 
 ##################################### Graficar #######################################
 
+Resistencia <- c(2158.7, 1678.15, 2316, 2061.3, 2207.5, 1708.3,1784.7)
+Edad <- c(15.5, 23.5, 8,17,5.5,19,24)
+datos <- data.frame(Resistencia, Edad)
+
+#install.packages("ggplot2", dependencies = TRUE)
 library(ggplot2)
-ggplot(datos, aes(x=fecha, y=cerra)) + geom_line(color="blue") + geom_point(color="red")
+ggplot(datos, aes(x=Edad, y=Resistencia)) + geom_point()
