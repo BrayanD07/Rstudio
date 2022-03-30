4bf62414a64c12b36c27d20a9895bbc503a6c633
BrayanD07 <52518417+BrayanD07@users.noreply.github.com>
2022-03-29 18:40:09 -0600

    Update Abrir CSV y graficar.R

--- Abrir CSV y graficar.R
+++ Abrir CSV y graficar.R
@@ -6,14 +6,8 @@ datos<- read.csv("BTC-USD.csv")
 #datos<- read.csv("C:\\Users\\Braya\\OneDrive\\Escritorio\\RepositorioR\\Rstudio\\BTC-USD.csv")
 
 print(datos)
-colnames(datosEntr)=c("fecha","abrio","maximo","minimo","cerra","adjcerra","volumen")
+colnames(datos)=c("fecha","abrio","maximo","minimo","cerró","adjcerró","volumen")
 
 ##################################### Graficar #######################################
-
-Resistencia <- c(2158.7, 1678.15, 2316, 2061.3, 2207.5, 1708.3,1784.7)
-Edad <- c(15.5, 23.5, 8,17,5.5,19,24)
-datos <- data.frame(Resistencia, Edad)
-
-#install.packages("ggplot2", dependencies = TRUE)
 library(ggplot2)
-ggplot(datos, aes(x=Edad, y=Resistencia)) + geom_point()
+ggplot(datos, aes(x=fecha, y=cerró)) + geom_line(color="blue") + geom_point(color="red")
