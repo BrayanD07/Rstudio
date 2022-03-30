fa2870b374951e87b2028b2d893d3024edc02777
BrayanD07 <52518417+BrayanD07@users.noreply.github.com>
2022-03-29 18:55:55 -0600

    Update Abrir y rotar imagenes.R

--- Abrir y rotar imagenes.R
+++ Abrir y rotar imagenes.R
@@ -1,25 +1,25 @@
 #install.packages("magick")
 #install.packages("rsvg")
 #install.packages("httpgd")
+
+#Llamaremos estas 2 librerias que nos permiten trabajar con imagenes
 library(magick)
 library(rsvg)
 
 ########################### ABRIR IMAGEN ###################################
 
+#Agregamos a una variable con la funcion image_read la imagen que espesifiquemos en la ruta entre parentesis 
 tiger <- image_read('velas.jpg')
+#Imprimimos la imagen para verificar que se haya guardado
 plot(tiger)
-#print(tiger)
-
-#file.show("https://github.com/Gerardo0873/Prolog2/blob/main/velas.jpg")
-
-imagen <- image_read("velas.jpg")
-#plot(imagen)
-
 
 ########################### ROTAR IMAGEN ###################################
 
+#Creamos un fondo a nuestra imagen para apreciar mejor la rotacion
 fondo <- image_border(image_background(imagen,"#6991ff"),"#298000","60x60")
+#Iniciamos los grados en 0
 grados <- 0
+#El for va a repetirse 30 veces cada un ciclo la imagen se va a rotar 10 grados con respecto al dondo
 for(i in 1:30){
   print(i)
   plot.new()
